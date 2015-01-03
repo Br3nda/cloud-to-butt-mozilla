6b14574246a189ddf0144bc4b3df71934f4916cd
Brenda Wallace <brenda@brenda.nz>
2015-01-03 15:03:11 +1300

    fixed tabs -> spaces

--- src/chrome/content/diet-to-yum.js
+++ src/chrome/content/diet-to-yum.js
@@ -2,31 +2,31 @@
 
 function walk(node) 
 {
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
+    
+    var child, next;
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-            if(node.parentElement.tagName.toLowerCase() != "script") {
-                handleText(node);
+    switch ( node.nodeType )  
+    {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while ( child ) 
+            {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
             }
-			break;
-	}
+            break;
+
+        case 3: // Text node
+                        if(node.parentElement.tagName.toLowerCase() != "script") {
+                            handleText(node);
+                        }
+            break;
+    }
 }
     
 function handleText(textNode) {
